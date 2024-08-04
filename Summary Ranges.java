e5125c55b9961bf6224c9fa453d2f2898e1a8f22
Kapilan <117897696+KapilanS@users.noreply.github.com>
2024-08-04 23:47:34 +0530

    Create Summary Ranges.java
    You are given a sorted unique integer array nums.
    
    A range [a,b] is the set of all integers from a to b (inclusive).
    
    Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
    
    Each range [a,b] in the list should be output as:
    
    "a->b" if a != b
    "a" if a == b
     
    
    Example 1:
    
    Input: nums = [0,1,2,4,5,7]
    Output: ["0->2","4->5","7"]
    Explanation: The ranges are:
    [0,2] --> "0->2"
    [4,5] --> "4->5"
    [7,7] --> "7"
    Example 2:
    
    Input: nums = [0,2,3,4,6,8,9]
    Output: ["0","2->4","6","8->9"]
    Explanation: The ranges are:
    [0,0] --> "0"
    [2,4] --> "2->4"
    [6,6] --> "6"
    [8,9] --> "8->9"
     
    
    Constraints:
    
    0 <= nums.length <= 20
    -231 <= nums[i] <= 231 - 1
    All the values of nums are unique.
    nums is sorted in ascending order.

---
+++ Summary Ranges.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result=new ArrayList();
+        if(nums.length==0){
+             return result;
+        }
+        int a=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(i==nums.length-1 || nums[i]+1 != nums[i+1]){
+                if(nums[i]!=a){
+                    result.add(a + "->" + nums[i]);
+                }
+                else{
+                    result.add(a + "");
+                }
+                if(i!=nums.length-1){
+                    a=nums[i+1];
+                }
+            }
+        } 
+        return result;
+    }
+}
